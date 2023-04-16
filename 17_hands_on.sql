dea34841f4bc750b71fe8eb7424169dbdb0b8cf2
Vighnesh Mane <86889795+vighnesh32@users.noreply.github.com>
2023-04-16 18:46:24 +0100

    Add files via upload

---
+++ 17_hands_on.sql
@@ -0,0 +1,10 @@
+/*--------------------------------------------
+1) Import share from Data Marketplace
+-- From standard listing: https://app.snowflake.com/marketplace/listing/GZ1M7Z2MQ39?search=OAG
+----------------------------------------------*/
+
+select *
+from public.OAG_schedule
+where DEPCTRY='US'
+and ARRCITY='PAR'
+and flight_date=current_date;
