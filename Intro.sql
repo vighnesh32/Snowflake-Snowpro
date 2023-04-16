5e6284be385c60dfb73106db4ac317a3645a4648
Vighnesh Mane <86889795+vighnesh32@users.noreply.github.com>
2023-04-16 19:53:20 +0100

    Add files via upload

---
+++ Intro.sql
@@ -0,0 +1,15 @@
+-- Command Line
+
+snowsql --Install downloaded version of SnowSQL
+
+cd .snowsql
+
+ls
+
+notepad config -- add account locator, cloud provider region, username & password to config file
+
+snowsql
+
+-- SnowSQL Command Line Tool
+
+SELECT CURRENT_VERSION();
